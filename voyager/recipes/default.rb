56549841aae099adc6ae2854281eedeb8b5b5d19
ravi-dhyani8881 <ravi.dhyani8881@gmail.com>
2024-08-22 17:14:38 +0100

    Update default.rb

--- voyager/recipes/default.rb
+++ voyager/recipes/default.rb
@@ -66,7 +66,7 @@ end
 template installDirectory + projectName + "/gql-server/schema.js" do
   source 'schema.js.erb'
   variables(
-    projectName: graph
+    graph: graph
   )
   owner 'ravi.dhyani'
   #group 'ravi.dhyani'
